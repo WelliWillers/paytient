b54eb9e1d09c85d15469f2dc58cd65b12d0a8e26
wellington <wellington@liquidworks.com.br>
2022-05-13 19:47:27 -0300

    iniciando projeto com next js configurando ambiente styles e fontes

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocuments extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet"/>
+
+                    <title>Paytient</title>
+                </Head>
+
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
